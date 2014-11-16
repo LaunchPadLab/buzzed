caa332616176750671bfb70c0c0b3ef7de26538b
Scott Weisman <scott@launchpadlab.com>
2014-11-16 09:50:28 -0600

    added .env

--- Gemfile
+++ Gemfile
@@ -3,4 +3,5 @@ ruby '2.1.3'
 
 gem "sinatra"
 gem 'thin'
+gem 'dotenv'
 
--- app.rb
+++ app.rb
@@ -1,9 +1,14 @@
 require 'rubygems'
 require 'bundler'
+require 'dotenv'
+Dotenv.load
 
 Bundler.require
 
 get '/' do
-  "Hello World!"
+  "Welcome to the Huron Door."
+  # send hipchat API request - msg to LPL room with link to let them in
+  # Play welcome message for visitor
+  # buzz in link is clicked in hipchat
 end
 
