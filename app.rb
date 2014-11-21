2f26c1db15c00aa2867654b37a801d1f943001e2
Scott Weisman <scott@launchpadlab.com>
2014-11-20 21:41:29 -0600

    added xml

--- app.rb
+++ app.rb
@@ -11,31 +11,32 @@ Dotenv.load
 Bundler.require
 
 get '/' do
+  send_to_hipchat
+  redirect to('/say-hello')
+end
 
-  response = Twilio::TwiML::Response.new do |r|
-    r.Say 'Hello, and welcome to Launch Pad Lab.', voice: 'alice'
-  end
-  puts response.text
-
-  # client = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']
-  # call_sid = client.account.calls.list({:status => 'in-progress' }).first.sid
-
+def send_to_hipchat
   client = HipChat::Client.new(ENV['HIPCHAT_TOKEN'], :api_version => 'v2')
   room = 'huron-door'
   username = 'scottweisman'
   buzzed_url = 'https://huron-door.herokuapp.com/buzzed'
-  client[room].send(username, "Someone is at the front door! <a href=#{buzzed_url?call_sid}>Let 'em in!</a>", color: 'green', message_format: 'html')
+  client[room].send(username, "Someone is at the front door! <a href=#{buzzed_url}>Let 'em in!</a>", color: 'green', message_format: 'html')
+end
 
+get '/say-hello' do
+  content_type 'text/xml'
+  Twilio::TwiML::Response.new do |r|
+    r.Say 'Hello, and welcome to Launch Pad Lab.', voice: 'alice'
+  end.text
 end
 
 get '/buzzed' do
+  content_type 'text/xml'
   # @call = @client.account.calls.get("CAe1644a7eed5088b159577c5802d8be38")
   # @call.update(:url => "http://demo.twilio.com/docs/voice.xml",
   #     :method => "POST")
   response = Twilio::TwiML::Response.new do |r|
     r.Play digits: "www6"
-  end
-  puts response.text
-
+  end.text
 end
 
