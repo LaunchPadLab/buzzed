eab46a9d9557061a2364446292286d602017a339
Scott Weisman <scott@launchpadlab.com>
2014-11-21 00:01:55 -0600

    testing room change

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ end
 
 def send_to_hipchat
   client = HipChat::Client.new(ENV['HIPCHAT_TOKEN'], :api_version => 'v2')
-  room = 'huron-door'
+  room = 'Huron Doors'
   username = 'scottweisman'
   buzzed_url = 'https://huron-door.herokuapp.com/buzzed'
   client[room].send(username, "Someone is at the front door! <a href=#{buzzed_url}>Let 'em in!</a>", color: 'green', message_format: 'html')
@@ -33,13 +33,13 @@ end
 
 get '/buzzed' do
   client = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']
-  calls = client.account.calls.list({:status => 'in-progress' })
+  calls = client.account.calls.list({ :status => 'in-progress' })
   if calls.any?
     current_call = client.account.calls.get(calls.first.sid)
     current_call.update(:url => "https://huron-door.herokuapp.com/buzz.xml", :method => "GET")
     "BUZZED!"
   else
-    "Something went wrong. Get off your ass and let them in."
+    "Something went wrong. Get off your butt and let them in."
   end
 end
 
