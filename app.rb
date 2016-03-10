9870c9c312cd41956c6ce6782e8d4008f9aa871c
Scott Weisman <scott@launchpadlab.com>
2016-03-10 13:04:27 -0600

    added auto

--- app.rb
+++ app.rb
@@ -44,13 +44,13 @@ end
 
 
 post '/' do
-  # if door_status == "auto"
-  #   bot.post(channel: '#launchpad-lab', username: 'buzzer', icon_emoji: ':door:', text: "Someone has been buzzed in.")
-  #   redirect to('/buzz-door')
-  # else
+  if redis.get("door_status") == "auto"
+    bot.post(channel: '#launchpad-lab', username: 'buzzer', icon_emoji: ':door:', text: "Someone has been buzzed in.")
+    redirect to('/buzz-door')
+  else
     bot.post(channel: '#launchpad-lab', username: 'buzzer', icon_emoji: ':door:', text: "Someone is at the front door.\nType *.open* to let them in.")
     redirect to('/say-hello')
-  # end
+  end
 end
 
 get '/say-hello' do
@@ -64,10 +64,6 @@ end
 post '/door-status' do
   bot.handle_item(params)
 end
-
-# post '/open' do
-#   redirect to('/buzz-door')
-# end
 
 post '/buzz-door' do
   if redis.get("door_status") == "open" || redis.get("door_status") == "auto"
