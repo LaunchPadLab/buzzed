6bc6ad92c48693b705c3389676d12de87769573d
Scott <scott@launchpadlab.com>
2021-06-17 18:35:03 -0500

    update twilio api formatting

--- app.rb
+++ app.rb
@@ -13,8 +13,8 @@ Dotenv.load
 Bundler.require
 
 config = {
-  'channel'          => '#the_office',
-  'name'             => 'buzzer',
+  'channel'          => "#the_office",
+  'name'             => "buzzer",
   'incoming_webhook' => ENV['INCOMING_WEBHOOK'],
   'outgoing_token'   => ENV['OUTGOING_TOKEN']
 }
@@ -31,7 +31,7 @@ bot = Slackbotsy::Bot.new(config) do
 
   hear /^.stayopen$/i do
     redis.set("door_status", "auto")
-    redis.expire("door_status", 300)
+    redis.expire("door_status", 3600)
     "The door will automatically buzz in for 5 minutes."
   end
 
@@ -42,37 +42,37 @@ bot = Slackbotsy::Bot.new(config) do
 
 end
 
-class Time
-  def is_weekday?
-    [1,2,3,4,5].include?(wday)
-  end
-end
+# class Time
+  # def is_weekday?
+  #   [1,2,3,4,5].include?(wday)
+  # end
+# end
 
 post '/' do
-  def office_open?
-    time = Time.now
-    time.is_weekday? && time.hour >= 9 && time.hour <= 17
-  end
+  # def office_open?
+    # time = Time.now
+    # time.is_weekday? && time.hour >= 9 && time.hour <= 17
+  # end
 
-  if redis.get("door_status") == "auto"
-    bot.post(channel: '#the_office', username: 'buzzer', icon_emoji: ':door:', text: "Someone has been buzzed in.")
-    content_type 'text/xml'
-    Twilio::TwiML::Response.new do |r|
-      r.Say 'Hello, and welcome to Launch Pad Lab.'
-      r.Play digits: 'wwww6'
-    end.text
-  else
-    bot.post(channel: '#the_office', username: 'buzzer', icon_emoji: ':door:', text: "Someone is at the front door.\nType *.open* to let them in.")
-    redirect to('/say-hello')
-  end
+  # if redis.get("door_status") == "auto"
+  #   bot.post(channel: "#the_office", username: "buzzer", icon_emoji: ":door:", text: "Someone has been buzzed in.")
+  #   content_type "text/xml"
+  #   Twilio::TwiML::Response.new do |r|
+  #     r.say(message: "Hello, and welcome to Launch Pad Lab.")
+  #     r.play(digits: "wwww6")
+  #   end.to_s
+  # else
+    bot.post(channel: "#the_office", username: "buzzer", icon_emoji: ":door:", text: "Someone is at the front door.\nType *.open* to let them in.")
+    redirect to("/say-hello")
+  # end
 end
 
 get '/say-hello' do
-  content_type 'text/xml'
-  Twilio::TwiML::Response.new do |r|
-    r.Say 'Hello, and welcome to Launch Pad Lab.'
-    r.Play '/such_great_heights.mp3'
-  end.text
+  content_type "text/xml"
+  Twilio::TwiML::VoiceResponse.new do |r|
+    r.say(message: "Hello, and welcome to Launch Pad Lab.")
+    r.play(url: "/such_great_heights.mp3")
+  end.to_s
 end
 
 post '/door-status' do
@@ -81,7 +81,7 @@ end
 
 post '/buzz-door' do
   client = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']
-  calls = client.account.calls.list({ :status => 'in-progress' })
+  calls = client.account.calls.list({ :status => "in-progress" })
   if calls.any?
     current_call = client.account.calls.get(calls.first.sid)
     current_call.update(:url => "https://buzzed-app.herokuapp.com/buzz.xml", :method => "GET")
